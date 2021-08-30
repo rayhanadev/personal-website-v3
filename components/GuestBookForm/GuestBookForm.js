a041fbd91bd7598531584bfeff1f991e13aba39e
Ray Arayilakath <72509475+RayhanADev@users.noreply.github.com>
2021-08-30 02:17:45 +0000

    fix: remove some guestbook code

--- components/GuestBookForm/GuestBookForm.js
+++ components/GuestBookForm/GuestBookForm.js
@@ -47,55 +47,5 @@ export default function GuestBookForm({ initialEntries }) {
     });
   };
 
-  return (
-    <>
-      <div className=>
-        <h5 className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
-          Sign the Guestbook
-        </h5>
-        <p className="my-1 text-gray-800 dark:text-gray-200">
-          Share a message for a future visitor of my site.
-        </p>
-        {user?.name ? (
-          <form className="relative my-4" onSubmit={leaveEntry}>
-            <input
-              ref={inputEl}
-              aria-label="Your message"
-              placeholder="Your message..."
-              required
-              className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-            />
-            <button
-              className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
-              type="submit"
-            >
-              {form.state === 'loading' ? <LoadingSpinner /> : 'Sign'}
-            </button>
-          </form>
-        ) : (
-          <a
-            className="flex items-center justify-center my-4 font-bold h-8 bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
-            href="/api/auth"
-          >
-            Login
-          </a>
-        )}
-        {form.state === 'error' ? (
-          <ErrorMessage>{form.message}</ErrorMessage>
-        ) : form.state === 'success' ? (
-          <SuccessMessage>{form.message}</SuccessMessage>
-        ) : (
-          <p className="text-sm text-gray-800 dark:text-gray-200">
-            Your information is only used to display your name and reply by
-            email.
-          </p>
-        )}
-      </div>
-      <div className="mt-4 space-y-8">
-        {entries?.map((entry) => (
-          <GuestbookEntry key={entry.id} entry={entry} user={user} />
-        ))}
-      </div>
-    </>
-  );
+  return '';
 }
